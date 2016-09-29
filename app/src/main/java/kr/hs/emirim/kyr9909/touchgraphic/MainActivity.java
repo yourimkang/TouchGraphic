6f7f42271444bfc2c1bb48852fa985c76c72477e
Yurim Kang <kyr9909@naver.com>
2016-09-29 19:11:30 +0900

    #2 Add String and Change Layout

--- app/src/main/java/kr/hs/emirim/kyr9909/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/touchgraphic/MainActivity.java
@@ -1,7 +1,9 @@
 package kr.hs.emirim.kyr9909.touchgraphic;
 
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,19 +16,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(new DrawShape(getApplicationContext()));
     }
-
     class DrawShape extends View{
         DrawShape(Context context){
             super(context);
+
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             float cx=getWidth()/2.0f;
-            float cy=getWidth()/2.0f;
+            float cy=getHeight()/2.0f;
             Paint paint=new Paint();
+            paint.setStrokeWidth(7);
+            paint.setColor(Color.BLUE);
+            paint.setStyle(Paint.Style.STROKE);
             canvas.drawCircle(cx,cy,50,paint);
+            paint.setColor(Color.MAGENTA);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawRect(10,10,210,160,paint);
+
         }
     }
 }
