006315a700f3c39eac54d4afcd2ffb41106dfd85
Yurim Kang <kyr9909@naver.com>
2016-09-29 20:16:32 +0900

    #5 Complete Event Programming

--- app/src/main/java/kr/hs/emirim/kyr9909/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/touchgraphic/MainActivity.java
@@ -11,8 +11,9 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    DrawShape ds = new DrawShape(getApplicationContext());
+    static final int LINE=1, RECT=2, CIRCLE=3;
+    int chooseShape=CIRCLE;
+    DrawShape ds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +22,19 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout linear=(LinearLayout)findViewById(R.id.linear_view);
         linear.addView(ds);
     }
-    public void drawShape(View view){
-
-
+    public void drawShape(View v){
+        switch (v.getId()){
+            case R.id.but_line:
+                chooseShape=LINE;
+                break;
+            case R.id.but_rect:
+                chooseShape=RECT;
+                break;
+            case R.id.but_cir:
+                chooseShape=CIRCLE;
+                break;
+        }
+        ds.invalidate();
     }
     class DrawShape extends View{
         DrawShape(Context context){
@@ -40,11 +51,19 @@ public class MainActivity extends AppCompatActivity {
             paint.setStrokeWidth(7);
             paint.setColor(Color.BLUE);
             paint.setStyle(Paint.Style.STROKE);
-            canvas.drawCircle(cx,cy,50,paint);
-            paint.setColor(Color.MAGENTA);
-            paint.setStyle(Paint.Style.FILL);
-            canvas.drawRect(10,10,210,160,paint);
-
+            switch (chooseShape){
+                case LINE:
+                    canvas.drawLine(50,100,650,100,paint);
+                    break;
+                case RECT:
+                    paint.setColor(Color.MAGENTA);
+                    paint.setStyle(Paint.Style.FILL);
+                    canvas.drawRect(10,10,210,160,paint);
+                    break;
+                case CIRCLE:
+                    canvas.drawCircle(cx,cy,50,paint);
+                    break;
+            }
         }
     }
 }
